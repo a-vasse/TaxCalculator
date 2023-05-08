7fd7bb652d9be17f972dde73180710935aa25476
Alex V <114860707+a-vasse@users.noreply.github.com>
2023-05-08 17:29:13 +0900

    Add files via upload

---
+++ TaxCalculatorApp/Pages/TaxCalculator/TaxCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaxCalculatorApp.Pages.TaxCalculator
+{
+    public class TaxCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
